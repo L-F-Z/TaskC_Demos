700a8b1c92ba36b0cd0c37f10c28a56c77849916
xxx1766 <xuqingru19@mails.ucas.ac.cn>
2024-09-18 15:05:21 +0800

    apptainer def file

--- Whisper/build.sh
+++ Whisper/build.sh
@@ -1,7 +1,7 @@
 # repo commit id
 commit ba3f3cd54b0e5b8ce1ab3de13e32122d0d5f98ab (HEAD -> main, origin/main, origin/HEAD)
 
-# CPU version
+# Docker CPU version
 docker build -f cpuDockerfile -t whisper:cpu .
 docker run -v ./test:/app/test \
      --rm \
@@ -11,7 +11,7 @@ docker run -v ./test:/app/test \
 
 rm ./test/res*
 
-# GPU version
+# Docker GPU version
 docker build -f gpuDockerfile -t whisper:gpu .
 docker run -v ./test:/app/test \
      --gpus all \
@@ -21,3 +21,11 @@ docker run -v ./test:/app/test \
      python3 /app/test/test.py
 
 rm ./test/res*
+
+# Apptainer CPU version
+apptainer build /tmp/whisper_cpu.sif ./cpuApptainer.def
+apptainer exec --bind ./test:/app/test /tmp/whisper_gpu.sif python3 /app/test/test.py
+
+# Apptainer GPU version
+apptainer build /tmp/whisper_gpu.sif ./gpuApptainer.def
+apptainer exec --nv --bind ./test:/app/test /tmp/whisper_gpu.sif python3 /app/test/test.py
