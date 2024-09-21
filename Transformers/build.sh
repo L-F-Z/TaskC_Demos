1272763510d7f6c60ad7a34e427a2d5bf16494fa
daiyongxuan <2423226609@qq.com>
2024-09-21 23:21:42 +0800

    Script: Add script to build image for transformer

--- Transformers/build.sh
+++ Transformers/build.sh
@@ -2,3 +2,5 @@
 git config --global http.postBuffer 1048576000
 
 commit 266d0a6375802075111ffb5e89e1b2a643ea44b8 (HEAD -> main, origin/main, origin/HEAD)
+
+docker build -f gpuDockerfile -t transformer ./test
